fc75889a9b89bfdec3dfc29676f846894b26e5c6
Maxim <konstantinovmax@gmail.com>
2021-08-09 20:20:10 +0300

    feat: сверстать две страницы

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { CardComponent } from './card/card.component';
+import { CardEditComponent } from './card-edit/card-edit.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CardComponent,
+    CardEditComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule
